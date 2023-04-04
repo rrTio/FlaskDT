019d8190a205c030eb36d9ccc75da9f12f25d3fd
rrTio <tio.rozsraven@ue.edu.ph>
2023-04-04 21:01:42 +0800

    Added comments

--- testDB.py
+++ testDB.py
@@ -2,32 +2,33 @@ import sqlite3
 import datetime
 import time
 
-delay = 1
+delay = 1 #delay in seconds
 
-def database(cur_time, cur_date):
+def database(cur_time, cur_date): 
     global conn, cursor
     conn = sqlite3.connect("DateTime.db")
     print ("Created Database")
 
     cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS `dt` (grade1 VARCHAR(255), grade2 VARCHAR(255), grade3 VARCHAR(255), reg_date TEXT, reg_time TEXT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS `dt` (grade1 VARCHAR(255), grade2 VARCHAR(255), grade3 VARCHAR(255), reg_date TEXT, reg_time TEXT)") #create database table
 
     if cursor.fetchone() is None:
-        cursor.execute("INSERT INTO `dt` VALUES(?,?,?,?,?)",("TEST1","TEST2","TEST3",cur_date,cur_time))
+        cursor.execute("INSERT INTO `dt` VALUES(?,?,?,?,?)",("TEST1","TEST2","TEST3",cur_date,cur_time)) #inserting data into the database table
         conn.commit()
         print("Insert successful")
 
 while True:
 
-    current = datetime.datetime.now()
+    current = datetime.datetime.now() #instantiation of date and time
 
-    cur_date = current.strftime("%m-%d-%Y")
-    cur_time = current.strftime("%H:%M:%S")
+    cur_date = current.strftime("%m-%d-%Y") #current date formatting
+    cur_time = current.strftime("%H:%M:%S") #current time formatting
 
+    #printing date and time
     print("DATE AND TIME")
     print("DATE: ", cur_date)
     print("TIME: ", cur_time)
     print(" ")
     
-    database(cur_time, cur_date)
-    time.sleep(delay) 
+    database(cur_time, cur_date) #calling function with parameters
+    time.sleep(delay) #delay before repeating next loop
