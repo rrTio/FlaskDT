a15674e8600e9e3197dca6faba78e05f14477967
rrTio <tio.rozsraven@ue.edu.ph>
2023-04-04 22:13:59 +0800

    Added open web browser function

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, make_response
+import webbrowser
 import sqlite3
 
 HOST_NAME = "localhost"
@@ -20,4 +21,5 @@ def index():
   return render_template("index.html", usr=users)
 
 if __name__ == "__main__":
+  webbrowser.open(HOST_NAME, ":", HOST_PORT)
   app.run(HOST_NAME, HOST_PORT)
